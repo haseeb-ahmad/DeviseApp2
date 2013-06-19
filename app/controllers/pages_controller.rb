6933be680b4c8449bb6867961a0f0b3a00a63122
Haseeb Ahmad <engg.haseeb@gmail.com>
2013-06-19 17:29:48 +0500

    Sign In Authentication & Sign Up page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,8 @@ class PagesController < ApplicationController
 
   def index_alt
   	unless user_signed_in?
-  		#redirect_to new_user_session_path
+  		redirect_to new_user_session_path
+  		return;
   	end
     render :layout => 'after_login'
   end
